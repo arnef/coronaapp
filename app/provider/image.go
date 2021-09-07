00af1cc69a407b854d473d531226b4dd7670f909
arnef <git@arnef.de>
2021-09-07 11:32:04 +0200

    🚸 show fallback image if qr code cannot be displayed

--- app/provider/image.go
+++ app/provider/image.go
@@ -3,12 +3,15 @@ package provider
 import (
 	"fmt"
 	"image"
+	"image/png"
 	"io/ioutil"
+	"os"
 	"path"
 
 	"github.com/arnef/coronaapp/app/storage"
 	"github.com/makiuchi-d/gozxing"
 	"github.com/makiuchi-d/gozxing/qrcode"
+	log "github.com/sirupsen/logrus"
 )
 
 var cache map[string]image.Image = map[string]image.Image{}
@@ -18,22 +21,41 @@ func ImageProvider(imgID string, width, height int) image.Image {
 		width = 512
 		height = 512
 	}
+	if cache == nil {
+		cache = map[string]image.Image{}
+	}
 	key := fmt.Sprintf("%s_%dx%d", imgID, width, height)
 	if _, exists := cache[key]; !exists {
 		writer := qrcode.NewQRCodeWriter()
 		name := fmt.Sprintf("%s.pem", imgID)
 		config, err := storage.UserConfigDir()
 		if err != nil {
-			// todo handle errors
-			panic(err)
+			return ErrorCert(err)
 		}
 		data, err := ioutil.ReadFile(path.Join(config, name))
 		if err != nil {
-			panic(err)
+			return ErrorCert(err)
 		}
-		img, _ := writer.Encode(string(data), gozxing.BarcodeFormat_QR_CODE, width, height, nil)
+		img, err := writer.Encode(string(data), gozxing.BarcodeFormat_QR_CODE, width, height, nil)
+		if err != nil {
+			return ErrorCert(err)
+		}
 		cache[key] = img
 	}
 
 	return cache[key]
+}
+
+func ErrorCert(err error) image.Image {
+	log.Error(err)
+	cat, err := os.Open("assets/bad_certs.png")
+	if err != nil {
+		panic(err)
+	}
+	img, err := png.Decode(cat)
+	if err != nil {
+		panic(err)
+	}
+	cat.Close()
+	return img
 }
